f32cc3c5d53c9e5765a596831f71461875ae16d9
Sebongile <sebomaduna26@gmail.com>
2023-10-16 06:40:54 -0700

    Changes

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
 
 /**
  * _printf - The function produces output according to a format.
@@ -8,3 +10,38 @@
  */
 int _printf(const char *format, ...)
 {
+int i;
+va_list ap;
+char *str;
+
+if (format == NULL)
+{
+	return -1;
+}
+
+va_start(ap, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+	if (format[i] == '%')
+	{
+		i++;
+		if (format[i] == 'c')
+		{
+			*str = va_arg(ap, char);
+			write(1, &str);
+		}
+		else if (format[i] == 's')
+		{
+			*str = va_arg(ap, char);
+			write(1, &str);
+		}
+		if (str == NULL)
+		{
+			str = "(null)";
+		}
+		return (strlen(str));
+	}
+}
+va_end(ap);
+return (0);
+}
