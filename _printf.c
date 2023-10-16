8941a1b3b1af547478aa1aaffbefb0d25ba697fd
Sebongile <sebomaduna26@gmail.com>
2023-10-16 04:39:50 -0700

    No 0

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _printf - The function produces output according to a format.
+ * @format: 
+ * Return: Number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
